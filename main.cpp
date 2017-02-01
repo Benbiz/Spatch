3b6a30b82f6903fa0e99efe60ac783db1c6edc9b
Benjamin Bizard <bizard_b@epitech.net>
2017-02-01 15:26:57 +0100

    First commit from netbeans

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Benjamin
+ *
+ * Created on 1 février 2017, 15:21
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=spatch
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/spatch
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=spatch.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/spatch.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=spatch
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/spatch
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=spatch.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/spatch.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
